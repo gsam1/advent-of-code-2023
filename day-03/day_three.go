5eab9a2e35a23ff6a04a387eb301d9a0d9f34bae
gsam1 <gsamardjiev@gmail.com>
2023-12-06 07:53:56 +0200

    Scanning them grids on day-03

--- day-03/day_three.go
+++ day-03/day_three.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
 func check(e error) {
@@ -17,9 +18,22 @@ func main() {
 	data, err := os.ReadFile("input.txt")
 	check(err)
 
-	for r, row := range strings.Split(string(data), "\n") {
+	initialDataGrid := strings.Split(string(data), "\n")
+	fmt.Printf("Rows: %d Columns %d \n", len(initialDataGrid), len(initialDataGrid[0]))
+	// fmt.Println(string(initialDataGrid[73][1]))
+
+	for r, row := range initialDataGrid {
 		for c, col := range row {
-			fmt.Println(c, r, string(col))
+			if !unicode.IsNumber(col) && string(col) != "." {
+				if r == 1 {
+					fmt.Println("Top row", c, col)
+				} else if r == len(initialDataGrid) {
+					fmt.Println("Bottom row", c, col)
+				}
+
+				// fmt.Println(r, c, string(col))
+			}
+			// fmt.Println(c, r, string(col))
 		}
 	}
 }
