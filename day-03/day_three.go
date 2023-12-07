96999d41cd9bd214cfaaeae71beb86b4c164801c
gsam1 <gsamardjiev@gmail.com>
2023-12-07 07:40:55 +0200

    Working on the grid

--- day-03/day_three.go
+++ day-03/day_three.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -11,6 +12,81 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func checkLeftRight(inputDataGrid []string, r int, c int, direction string) string {
+	numberStr := ""
+	sourceNum := inputDataGrid[r][c]
+	increment := 0
+
+	lr := 1
+	if direction == "left" {
+		lr = -1
+	}
+
+	for unicode.IsNumber(rune(sourceNum)) {
+		numberStr += string(sourceNum)
+		sourceNum = inputDataGrid[r][c+lr*(1+increment)]
+		increment += 1
+	}
+
+	return numberStr
+}
+
+func checkBottomTop(inputDataGrid []string, r int, c int, direction string) string {
+	numberStr := ""
+	sourceNum := inputDataGrid[r][c]
+	increment := 0
+
+	lc := 1
+	if direction == "left" {
+		lc = -1
+	}
+
+	for unicode.IsNumber(rune(sourceNum)) {
+		numberStr += string(sourceNum)
+		sourceNum = inputDataGrid[r+lc*(1+increment)][c]
+		increment += 1
+	}
+
+	return numberStr
+}
+
+func scanGrid(inputDataGrid []string, r int, c int) int {
+	left := inputDataGrid[r][c-1]
+	right := inputDataGrid[r][c+1]
+	numbers := map[string]string{}
+
+	if r == 1 {
+		bottom := inputDataGrid[r+1][c]
+		dlbottom := inputDataGrid[r+1][c-1]
+		drbottom := inputDataGrid[r+1][c+1]
+
+		// check left
+		if unicode.IsNumber(rune(left)) {
+			numbers[strconv.Itoa(r)+strconv.Itoa(c-1)] = checkLeftRight(inputDataGrid, c, r, "left")
+		}
+		// check right
+		if unicode.IsNumber(rune(right)) {
+			numbers[strconv.Itoa(r)+strconv.Itoa(c-1)] = checkLeftRight(inputDataGrid, c, r, "right")
+		}
+		// check r + 1
+		if unicode.IsNumber(rune(bottom)) {
+			numbers[strconv.Itoa(r+1)+strconv.Itoa(c+1)] = numberStr
+		}
+
+	} else if r == len(inputDataGrid) {
+		// check left
+		// check right
+		// check r - 1
+	} else {
+		// check left
+		// check right
+		// check r - 1
+		// check r + 1
+
+	}
+
 }
 
 // grid stuff
@@ -25,15 +101,8 @@ func main() {
 	for r, row := range initialDataGrid {
 		for c, col := range row {
 			if !unicode.IsNumber(col) && string(col) != "." {
-				if r == 1 {
-					fmt.Println("Top row", c, col)
-				} else if r == len(initialDataGrid) {
-					fmt.Println("Bottom row", c, col)
-				}
-
-				// fmt.Println(r, c, string(col))
+				scanGrid(initialDataGrid, r, c)
 			}
-			// fmt.Println(c, r, string(col))
 		}
 	}
 }
