deef4aa0b4a58822a0d01500b606c66f9fad30c4
gsam1 <gsamardjiev@gmail.com>
2023-12-08 08:27:10 +0200

    Updated code for day 3

--- day-03/day_three.go
+++ day-03/day_three.go
@@ -45,62 +45,64 @@ func checkLeftRight(inputDataGrid []string, c int, r int, direction string) stri
 }
 
 func scanGrid(inputDataGrid []string, r int, c int, mode string) map[string]string {
-	left := inputDataGrid[r][c-1]
-	right := inputDataGrid[r][c+1]
 	numbers := map[string]string{}
 
-	below := inputDataGrid[r+1][c]
-	up := inputDataGrid[r-1][c]
-	dlbelow := inputDataGrid[r+1][c-1]
-	drbelow := inputDataGrid[r+1][c+1]
-	dlup := inputDataGrid[r-1][c-1]
-	drup := inputDataGrid[r-1][c+1]
+	if c > 0 || c < len(inputDataGrid[r])-1 {
+		left := inputDataGrid[r][c-1]
+		right := inputDataGrid[r][c+1]
 
-	// check left
-	if unicode.IsNumber(rune(left)) {
-		index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-		fmt.Println("Left is number.", index)
-		numbers[index] = checkLeftRight(inputDataGrid, c-1, r, "left")
-	}
-	// check right
-	if unicode.IsNumber(rune(right)) {
-		index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
-		fmt.Println("Right is number.", index)
-		numbers[index] = checkLeftRight(inputDataGrid, c+1, r, "right")
+		below := inputDataGrid[r+1][c]
+		up := inputDataGrid[r-1][c]
+		dlbelow := inputDataGrid[r+1][c-1]
+		drbelow := inputDataGrid[r+1][c+1]
+		dlup := inputDataGrid[r-1][c-1]
+		drup := inputDataGrid[r-1][c+1]
 
-	}
-
-	if mode == "normal" || mode == "first" {
-		if unicode.IsNumber(rune(below)) {
-			index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c), 10)
-			fmt.Println("Below is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c, r+1, "left") + checkLeftRight(inputDataGrid, c, r+1, "right")
-		} else if unicode.IsNumber(rune(dlbelow)) {
+		// check left
+		if unicode.IsNumber(rune(left)) {
 			index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-			fmt.Println("Diagonal Left Below is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c-1, r+1, "left") + checkLeftRight(inputDataGrid, c-1, r+1, "right")
-		} else if unicode.IsNumber(rune(drbelow)) {
-			index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
-			fmt.Println("Diagonal Right Below is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c+1, r+1, "left") + checkLeftRight(inputDataGrid, c+1, r+1, "right")
+			// fmt.Println("Left is number.", index)
+			numbers[index] = checkLeftRight(inputDataGrid, c-1, r, "left")
 		}
-	}
-	// check below
+		// check right
+		if unicode.IsNumber(rune(right)) {
+			index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
+			// fmt.Println("Right is number.", index)
+			numbers[index] = checkLeftRight(inputDataGrid, c+1, r, "right")
 
-	if mode == "normal" || mode == "last" {
-		// check up
-		if unicode.IsNumber(rune(up)) {
-			index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-			fmt.Println("Up is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c, r-1, "left") + checkLeftRight(inputDataGrid, c, r-1, "right")
-		} else if unicode.IsNumber(rune(dlup)) {
-			index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-			fmt.Println("Diagonal Up Left is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c-1, r-1, "left") + checkLeftRight(inputDataGrid, c-1, r-1, "right")
-		} else if unicode.IsNumber(rune(drup)) {
-			index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
-			fmt.Println("Diagonal Up Right is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c+1, r-1, "left") + checkLeftRight(inputDataGrid, c+1, r-1, "right")
+		}
+
+		if mode == "normal" || mode == "first" {
+			if unicode.IsNumber(rune(below)) {
+				index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c), 10)
+				// fmt.Println("Below is number.", index)
+				numbers[index] = checkLeftRight(inputDataGrid, c-1, r+1, "left") + string(below) + checkLeftRight(inputDataGrid, c+1, r+1, "right")
+			} else if unicode.IsNumber(rune(dlbelow)) {
+				index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
+				// fmt.Println("Diagonal Left Below is number.", index)
+				numbers[index] = checkLeftRight(inputDataGrid, c-1, r+1, "left")
+			} else if unicode.IsNumber(rune(drbelow)) {
+				index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
+				// fmt.Println("Diagonal Right Below is number.", index)
+				numbers[index] = checkLeftRight(inputDataGrid, c+1, r+1, "right")
+			}
+		}
+
+		if mode == "normal" || mode == "last" {
+			// check up
+			if unicode.IsNumber(rune(up)) {
+				index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
+				// fmt.Println("Up is number.", index)
+				numbers[index] = checkLeftRight(inputDataGrid, c-1, r-1, "left") + string(up) + checkLeftRight(inputDataGrid, c+1, r-1, "right")
+			} else if unicode.IsNumber(rune(dlup)) {
+				index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
+				// fmt.Println("Diagonal Up Left is number.", index)
+				numbers[index] = checkLeftRight(inputDataGrid, c-1, r-1, "left")
+			} else if unicode.IsNumber(rune(drup)) {
+				index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
+				// fmt.Println("Diagonal Up Right is number.", index)
+				numbers[index] = checkLeftRight(inputDataGrid, c+1, r-1, "right")
+			}
 		}
 	}
 
@@ -121,7 +123,7 @@ func main() {
 	for r, row := range initialDataGrid {
 		for c, col := range row {
 			if !unicode.IsNumber(col) && string(col) != "." {
-				fmt.Printf("!!!!! Condition met: %s r:%d c:%d \n", string(col), r, c)
+				// fmt.Printf("!!!!! Condition met: %s r:%d c:%d \n", string(col), r, c)
 				mode := "normal"
 				if r == 0 {
 					mode = "first"
@@ -134,10 +136,6 @@ func main() {
 					numbers[k] = v
 				}
 			}
-		}
-
-		if r == 2 {
-			break
 		}
 	}
 
