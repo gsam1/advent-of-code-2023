a75193edd1c17ed5258b255d98e5d2dffea984d3
gsam1 <gsamardjiev@gmail.com>
2023-12-09 09:06:27 +0200

    Solved! Slow though :D

--- day-03/day_three.go
+++ day-03/day_three.go
@@ -8,6 +8,16 @@ import (
 	"unicode"
 )
 
+// 544433
+// 76314915
+
+type Part struct {
+	row    int
+	column int
+	symbol string
+	value  []string
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -31,10 +41,17 @@ func checkLeftRight(inputDataGrid []string, c int, r int, direction string) stri
 		lr = -1
 	}
 
-	for unicode.IsNumber(rune(sourceNum)) {
+	condition := unicode.IsNumber(rune(sourceNum))
+	for condition {
 		numberStr += string(sourceNum)
-		sourceNum = inputDataGrid[r][c+lr*(1+increment)]
-		increment += 1
+		column := c + lr*(1+increment)
+		if column < 0 || column > len(inputDataGrid[r])-1 {
+			condition = false
+		} else {
+			sourceNum = inputDataGrid[r][c+lr*(1+increment)]
+			condition = unicode.IsNumber(rune(sourceNum))
+			increment += 1
+		}
 	}
 
 	if direction == "left" {
@@ -44,64 +61,52 @@ func checkLeftRight(inputDataGrid []string, c int, r int, direction string) stri
 	return numberStr
 }
 
-func scanGrid(inputDataGrid []string, r int, c int, mode string) map[string]string {
-	numbers := map[string]string{}
+func scanGridStr(inputDataGrid []string, r int, c int, mode string) []string {
+	var numbers []string
 
-	if c > 0 || c < len(inputDataGrid[r])-1 {
-		left := inputDataGrid[r][c-1]
-		right := inputDataGrid[r][c+1]
+	left := inputDataGrid[r][c-1]
+	right := inputDataGrid[r][c+1]
 
-		below := inputDataGrid[r+1][c]
-		up := inputDataGrid[r-1][c]
-		dlbelow := inputDataGrid[r+1][c-1]
-		drbelow := inputDataGrid[r+1][c+1]
-		dlup := inputDataGrid[r-1][c-1]
-		drup := inputDataGrid[r-1][c+1]
+	below := inputDataGrid[r+1][c]
+	up := inputDataGrid[r-1][c]
+	dlbelow := inputDataGrid[r+1][c-1]
+	drbelow := inputDataGrid[r+1][c+1]
+	dlup := inputDataGrid[r-1][c-1]
+	drup := inputDataGrid[r-1][c+1]
 
-		// check left
-		if unicode.IsNumber(rune(left)) {
-			index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-			// fmt.Println("Left is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c-1, r, "left")
-		}
-		// check right
-		if unicode.IsNumber(rune(right)) {
-			index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
-			// fmt.Println("Right is number.", index)
-			numbers[index] = checkLeftRight(inputDataGrid, c+1, r, "right")
+	// check left
+	if unicode.IsNumber(rune(left)) {
+		numbers = append(numbers, checkLeftRight(inputDataGrid, c-1, r, "left"))
+	}
+	// check right
+	if unicode.IsNumber(rune(right)) {
+		numbers = append(numbers, checkLeftRight(inputDataGrid, c+1, r, "right"))
 
-		}
+	}
 
-		if mode == "normal" || mode == "first" {
-			if unicode.IsNumber(rune(below)) {
-				index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c), 10)
-				// fmt.Println("Below is number.", index)
-				numbers[index] = checkLeftRight(inputDataGrid, c-1, r+1, "left") + string(below) + checkLeftRight(inputDataGrid, c+1, r+1, "right")
-			} else if unicode.IsNumber(rune(dlbelow)) {
-				index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-				// fmt.Println("Diagonal Left Below is number.", index)
-				numbers[index] = checkLeftRight(inputDataGrid, c-1, r+1, "left")
-			} else if unicode.IsNumber(rune(drbelow)) {
-				index := "r" + strconv.FormatInt(int64(r), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
-				// fmt.Println("Diagonal Right Below is number.", index)
-				numbers[index] = checkLeftRight(inputDataGrid, c+1, r+1, "right")
+	if mode == "normal" || mode == "first" {
+		if unicode.IsNumber(rune(below)) {
+			numbers = append(numbers, checkLeftRight(inputDataGrid, c-1, r+1, "left")+string(below)+checkLeftRight(inputDataGrid, c+1, r+1, "right"))
+		} else {
+			if unicode.IsNumber(rune(dlbelow)) {
+				numbers = append(numbers, checkLeftRight(inputDataGrid, c-1, r+1, "left"))
+			}
+			if unicode.IsNumber(rune(drbelow)) {
+				numbers = append(numbers, checkLeftRight(inputDataGrid, c+1, r+1, "right"))
 			}
 		}
+	}
 
-		if mode == "normal" || mode == "last" {
-			// check up
-			if unicode.IsNumber(rune(up)) {
-				index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-				// fmt.Println("Up is number.", index)
-				numbers[index] = checkLeftRight(inputDataGrid, c-1, r-1, "left") + string(up) + checkLeftRight(inputDataGrid, c+1, r-1, "right")
-			} else if unicode.IsNumber(rune(dlup)) {
-				index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c-1), 10)
-				// fmt.Println("Diagonal Up Left is number.", index)
-				numbers[index] = checkLeftRight(inputDataGrid, c-1, r-1, "left")
-			} else if unicode.IsNumber(rune(drup)) {
-				index := "r" + strconv.FormatInt(int64(r-1), 10) + "c" + strconv.FormatInt(int64(c+1), 10)
-				// fmt.Println("Diagonal Up Right is number.", index)
-				numbers[index] = checkLeftRight(inputDataGrid, c+1, r-1, "right")
+	if mode == "normal" || mode == "last" {
+		// check up
+		if unicode.IsNumber(rune(up)) {
+			numbers = append(numbers, checkLeftRight(inputDataGrid, c-1, r-1, "left")+string(up)+checkLeftRight(inputDataGrid, c+1, r-1, "right"))
+		} else {
+			if unicode.IsNumber(rune(dlup)) {
+				numbers = append(numbers, checkLeftRight(inputDataGrid, c-1, r-1, "left"))
+			}
+			if unicode.IsNumber(rune(drup)) {
+				numbers = append(numbers, checkLeftRight(inputDataGrid, c+1, r-1, "right"))
 			}
 		}
 	}
@@ -117,10 +122,12 @@ func main() {
 
 	initialDataGrid := strings.Split(string(data), "\n")
 	fmt.Printf("Rows: %d Columns %d \n", len(initialDataGrid), len(initialDataGrid[0]))
-	numbers := map[string]string{}
+	var numbers []Part
 	// fmt.Println(string(initialDataGrid[73][1]))
 
+	fmt.Println("----Check")
 	for r, row := range initialDataGrid {
+		var partNum [][]string
 		for c, col := range row {
 			if !unicode.IsNumber(col) && string(col) != "." {
 				// fmt.Printf("!!!!! Condition met: %s r:%d c:%d \n", string(col), r, c)
@@ -131,17 +138,26 @@ func main() {
 				if r == len(initialDataGrid)-1 {
 					mode = "last"
 				}
-				res_scan := scanGrid(initialDataGrid, r, c, mode)
-				for k, v := range res_scan {
-					numbers[k] = v
-				}
+				newPart := Part{row: r, column: c, symbol: string(col), value: scanGridStr(initialDataGrid, r, c, mode)}
+				partNum = append(partNum, newPart.value)
+				numbers = append(numbers, newPart)
 			}
 		}
+		fmt.Println(row, partNum)
 	}
 
+	totalSum := 0
+	for _, v := range numbers {
+		tmpSum := 0
+		for _, vv := range v.value {
+			val, err := strconv.Atoi(vv)
+			check(err)
+			tmpSum += val
+		}
+		// fmt.Println(v, tmpSum)
+		totalSum += tmpSum
+
+	}
 	fmt.Println("----Results")
-	for k, v := range numbers {
-		fmt.Println(k, v)
-	}
-
+	fmt.Println(totalSum)
 }
